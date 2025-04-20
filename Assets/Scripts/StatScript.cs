61fefb287d573d467e5d5ecb9b2e88ff650dee05
RavenJae21 <ammeza042104@gmail.com>
2025-04-20 12:35:00 -0500

    Edited Stats Script
    - added variables a a stat function
    - the script is not functionable as we don't want the system to constantly add to the stats every frame

--- Assets/Scripts/StatScript.cs
+++ Assets/Scripts/StatScript.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class StatScript : MonoBehaviour
 {
-    //create player stats (health/damage/speed)
-    //create enemy stats
+    //Player Stats
+    public int playerHealth;
+    public int playerDamage;
+    public int playerSpeed;
+
+    //Enemy Stats
+     public int enemyHealth;
+    public int enemyDamage;
+    public int enemySpeed;
+
+    //Other
+    public bool didWeWin;
+    
+    
 
     //if player wins -> add to player stats & add to enemy stats
     //if player loses -> reset player & enemy stats
@@ -11,12 +23,51 @@ public class StatScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        didWeWin = false;
+
+        playerHealth = 50;
+        playerDamage = 50;
+        playerSpeed = 50;
+
+        enemyHealth = 50;
+        enemyDamage = 50;
+        enemySpeed = 50;
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Stats();
     }
+
+    void Stats()
+    {
+        if didWeWin == true
+        {
+            //add to player stats
+            playerHealth += 10;
+            playerDamage += 10;
+            playerSpeed += 10;
+
+            //add to enemy stats
+            enemyHealth += 10;
+            enemyDamage += 10;
+            enemySpeed += 10;
+        }
+        else if didWeWin == false
+        {
+            //reset player stats
+            playerHealth = 50;
+            playerDamage = 50;
+            playerSpeed = 50;
+
+            //reset enemy stats
+            enemyHealth = 50;
+            enemyDamage = 50;
+            enemySpeed = 50;
+        }
+    }
+
 }
