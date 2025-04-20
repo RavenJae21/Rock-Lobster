a50e93dd9be80f647682be891a74352449a1caa7
RavenJae21 <ammeza042104@gmail.com>
2025-04-19 20:48:34 -0500

    Created Stat Script
    - only pseudo coded

---
+++ Assets/Scripts/StatScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class StatScript : MonoBehaviour
+{
+    //create player stats (health/damage/speed)
+    //create enemy stats
+
+    //if player wins -> add to player stats & add to enemy stats
+    //if player loses -> reset player & enemy stats
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
