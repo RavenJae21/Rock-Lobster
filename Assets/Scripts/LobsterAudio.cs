96e05d38611d0164e30f50054b6c4c6071235228
Josh-VGAM <jrodr148@stedwards.edu>
2025-04-22 11:55:14 -0500

    LOBSTER AUIDO COMPLETE

--- Assets/Scripts/LobsterAudio.cs
+++ Assets/Scripts/LobsterAudio.cs
@@ -1,15 +1,22 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LobsterAudio : MonoBehaviour
 {
     public GameObject luis;
 
-    public AudioSource luisHurt;
     public AudioSource luisWalk;
+    public AudioSource luisHit;
+
+    void Start()
+    {
+        luisWalk.Stop();
+        luisHit.Stop();
+    }
 
     void Update()
     {
-        // Check if any of the WASD keys are being pressed
+// Check if any of the WASD keys are being pressed
         bool isMoving = Input.GetKey(KeyCode.W) || 
                         Input.GetKey(KeyCode.A) || 
                         Input.GetKey(KeyCode.S) || 
@@ -27,6 +34,24 @@ public class LobsterAudio : MonoBehaviour
             if(luisWalk.isPlaying)
             {
                 luisWalk.Stop();
+            }
+        }
+
+//Check if left click is being pressed
+        bool isAttacking = Input.GetKey(KeyCode.Mouse0);
+
+        if(isAttacking)
+        {
+            if(!luisHit.isPlaying)
+            {
+                luisHit.Play();
+            }
+        }
+        else
+        {
+            if(luisHit.isPlaying)
+            {
+                luisHit.Stop();
             }
         }
     }
